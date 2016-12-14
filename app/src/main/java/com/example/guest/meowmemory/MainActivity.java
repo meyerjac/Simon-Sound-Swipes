a51d3b2114a2116ec47ffb6eb14d38326cc62051
Jackson Meyer and Yusuf Qedan <jackson@>
2016-12-14 10:59:10 -0800

    add new UI thread, fixed bugs and has delay for the the timer

--- app/src/main/java/com/example/guest/meowmemory/MainActivity.java
+++ app/src/main/java/com/example/guest/meowmemory/MainActivity.java
@@ -5,12 +5,15 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.Image;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.guest.meowmemory.util.Android_Gesture_Detector;
@@ -20,12 +23,11 @@ import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity implements SensorEventListener, View.OnClickListener {
     private String TAG = MainActivity.class.getSimpleName();
-    private ImageView mCat;
-    private ImageView mWookie;
-    private ImageView mDog;
-    private ImageView mSponge;
+    private Button mNewGame;
+    private ImageView mRedLight;
+    private ImageView mGreenLight;
     private MediaPlayer cat;
     private MediaPlayer wookie;
     private MediaPlayer dog;
@@ -46,32 +48,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private int guess;
     private int patternIndex;
     private int i = 0;
+    private boolean simonSpeaking = true;
 
-    Context context;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mCat = (ImageView) findViewById(R.id.cat);
-        mWookie = (ImageView) findViewById(R.id.wookie);
-        mDog = (ImageView) findViewById(R.id.dog);
-        mSponge= (ImageView) findViewById(R.id.shaggy);
-        context = this;
+
+        mNewGame = (Button) findViewById(R.id.newGame);
+        mGreenLight = (ImageView) findViewById(R.id.green);
+        mRedLight = (ImageView) findViewById(R.id.red);
+
         cat = MediaPlayer.create(this, R.raw.meow);
         dog = MediaPlayer.create(this, R.raw.bark);
         wookie = MediaPlayer.create(this, R.raw.chewy);
-        cat = MediaPlayer.create(this, R.raw.meow);
         shake = MediaPlayer.create(this, R.raw.shake);
         sponge = MediaPlayer.create(this, R.raw.sponge);
 
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mSensorManager.registerListener(this, mSensor, mSensorManager.SENSOR_DELAY_NORMAL);
+//        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+//        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+//        mSensorManager.registerListener(this, mSensor, mSensorManager.SENSOR_DELAY_NORMAL);
 
-        flash();
+        context = this;
+        mNewGame.setOnClickListener(this);
 
-        Android_Gesture_Detector custom_gesture_detector = new Android_Gesture_Detector(){
+        Android_Gesture_Detector custom_gesture_detector = new Android_Gesture_Detector() {
             @Override
             public void onSwipeUp() {
                 guess = 1;
@@ -97,7 +100,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 sponge.start();
             }
         };
-        mGestureDetector = new GestureDetector(this,custom_gesture_detector);
+        mGestureDetector = new GestureDetector(this, custom_gesture_detector);
 
     }
 
@@ -119,7 +122,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 float speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
 
                 if (speed > SHAKE_THRESHOLD) {
-//                    guess = 0;
+                    guess = 0;
 //                    evaluateGuess();
 //                    onShake();
 
@@ -138,11 +141,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        mGestureDetector.onTouchEvent(event);
-
-        return super.onTouchEvent(event);
-        // Return true if you have consumed the event, false if you haven't.
-        // The default implementation always returns false.
+        if (!simonSpeaking) {
+            mGestureDetector.onTouchEvent(event);
+            return super.onTouchEvent(event);
+        } else {
+            return false;
+        }
     }
 
     public void onShake() {
@@ -151,13 +155,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         shake.start();
     }
 
-    public void evaluateGuess(){
+    public void evaluateGuess() {
         if (guess == pattern.get(patternIndex)) {
             patternIndex++;
-        }
-        else{
+        } else {
             Log.d(TAG, "You Lose!");
             patternIndex = 0;
+            timer.cancel();
+            timer.purge();
+
             pattern.removeAll(pattern);
         }
 
@@ -167,46 +173,74 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             TimerTask timerTask = new TimerTask() {
                 @Override
                 public void run() {
-                    flash();
+                    startSequence();
                     timerAfterAnswer.cancel();
                     timerAfterAnswer.purge();
                 }
             };
-            timerAfterAnswer.schedule(timerTask, 0, 1000);
+            timerAfterAnswer.schedule(timerTask, 1000, 1);
         }
     }
 
-    public void flash(){
-        pattern.add(rand.nextInt(3) + 1);
+    public void startSequence() {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mRedLight.setVisibility(View.VISIBLE);
+                mGreenLight.setVisibility(View.INVISIBLE);
+            }
+        });
+        pattern.add(rand.nextInt(4) + 1);
         timer = new Timer();
-        TimerTask timerTask = new TimerTask(){
+        TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
                 Log.d(TAG, "move is:" + pattern.get(i));
                 playSound(pattern.get(i));
                 i++;
-                if (i == pattern.size()){
+                if (i == pattern.size()) {
                     i = 0;
+                    simonSpeaking = false;
+                    resetLight();
                     timer.cancel();
                     timer.purge();
                 }
             }
         };
 
-        timer.schedule(timerTask, 0, 1000);
+        timer.schedule(timerTask, 1000, 1200);
+        simonSpeaking = true;
     }
 
-    private void playSound(int soundId){
+    private void playSound(int soundId) {
         if (soundId == 0) {
             shake.start();
-        }else if(soundId == 1){
+        } else if (soundId == 1) {
             cat.start();
-        }else if(soundId == 2){
+        } else if (soundId == 2) {
             wookie.start();
-        }else if(soundId == 3){
+        } else if (soundId == 3) {
             dog.start();
-        }else if(soundId == 4){
+        } else if (soundId == 4) {
             sponge.start();
         }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mNewGame) {
+            Log.d(TAG, "onClick: ");
+            startSequence();
+        }
+    }
+
+    private void resetLight(){
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mGreenLight.setVisibility(View.VISIBLE);
+                mRedLight.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/guest/meowmemory/ui/RulesActivity.java
@@ -0,0 +1,15 @@
+package com.example.guest.meowmemory.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.guest.meowmemory.R;
+
+public class RulesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
+    }
+}
